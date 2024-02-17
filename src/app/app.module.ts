45f157ca2d24a642b79d94427dd52b93225f698c
Sean Hagen <sean.r.hagen@gmail.com>
2024-02-17 14:39:34 -0600

    Made into PWA, changed grid sizing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ParentComponent } from './parent/parent.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,13 @@ import { ParentComponent } from './parent/parent.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ParentComponent
+    ParentComponent,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
